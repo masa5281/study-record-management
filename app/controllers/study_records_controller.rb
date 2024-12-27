15f55685093efcc7bf3497928897dfd1605d6cea
masa5281 <masa.h.5281@gmail.com>
2024-12-28 06:55:04 +0900

    Add: 新規投稿ページを作成（#7）

--- app/controllers/study_records_controller.rb
+++ app/controllers/study_records_controller.rb
@@ -1,2 +1,19 @@
 class StudyRecordsController < ApplicationController
+  def index
+  end
+
+  def new
+    @study_record = StudyRecord.new
+  end
+
+  def create
+    @study_record = StudyRecord.new(study_record_params)
+    @study_record.save
+    redirect_to study_records_path
+  end
+
+  private
+  def study_record_params
+    params.require(:study_record).permit(:name, :content)
+  end
 end
