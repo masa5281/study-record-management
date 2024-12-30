e9daa9bc8279ddfc9b18febb3410ea3eedbae7c0
masa5281 <masa.h.5281@gmail.com>
2024-12-30 13:21:23 +0900

    Add: deviseの初期セットアップを完了（#11）
    - 設定ファイルを作成
    - Userモデルを作成
    - usersテーブルのマイグレーション実行

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "ostruct"
 gem "ruby-lsp"
 gem "sass-rails"
 gem "rails-i18n"
+gem "devise"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_26_224713) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_29_224329) do
   create_table "study_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
