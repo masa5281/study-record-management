6a9e49e3470e607ca03fe7adc03f9e2e98a8477d
masa5281 <masa.h.5281@gmail.com>
2025-01-02 08:05:52 +0900

    Add: 新規ユーザー登録画面に「username」入力欄を追加し、登録処理を実装（#11）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # registrations_controller.rbで記述した内容を有効化
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
