9e6b1997ef265e489aab260608db55340ec31d68
masa5281 <masa.h.5281@gmail.com>
2025-01-04 06:11:51 +0900

    Add: study_recordsテーブルとtagsテーブルのアソシエーションを定義（#14）
    - 中間テーブルのstudy_record_tagsを経由する

--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -1,5 +1,7 @@
 class StudyRecord < ApplicationRecord
   belongs_to :user
+  has_many :tags, through: :study_record_tags
+  has_many :study_record_tags
 
   validates :name, presence: true
   validates :content, presence: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :study_records, through: :study_record_tags
+  has_many :study_record_tags
 end
