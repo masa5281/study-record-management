a4d7bd7a8f84bdbb116c6cb1f996cae5942cb5e5
masa5281 <masa.h.5281@gmail.com>
2025-01-03 17:33:52 +0900

    Add: Tagモデルとtagsテーブルを作成（#14）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_30_045431) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_03_075841) do
   create_table "study_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_30_045431) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_study_records_on_user_id"
+  end
+
+  create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
