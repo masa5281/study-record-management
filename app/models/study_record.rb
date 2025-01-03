591edaef91bdc12e9b0ed090e82f9bbd10fb7adc
masa5281 <masa.h.5281@gmail.com>
2025-01-04 06:22:52 +0900

    Add: study_recordsテーブルとtagsテーブルのにdependentオプションを設定（#14）

--- app/models/study_record.rb
+++ app/models/study_record.rb
@@ -1,7 +1,7 @@
 class StudyRecord < ApplicationRecord
   belongs_to :user
   has_many :tags, through: :study_record_tags
-  has_many :study_record_tags
+  has_many :study_record_tags, dependent: :destroy
 
   validates :name, presence: true
   validates :content, presence: true
