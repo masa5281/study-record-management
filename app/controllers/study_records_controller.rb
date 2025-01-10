45eca343af512c4baeab282461374447d8238573
masa5281 <masa.h.5281@gmail.com>
2025-01-11 06:55:21 +0900

    Add: 学習記録のフラッシュメッセージを実装（#19）
    - 学習記録の新規作成、編集、削除時に成功・失敗メッセージを表示
      - 削除時は成功メッセージのみ表示

---
+++ app/controllers/study_records_controller.rb
@@ -0,0 +1,64 @@
+class StudyRecordsController < ApplicationController
+  before_action :set_current_user
+  before_action :set_study_record, only: %i[show edit update destroy]
+  before_action :ensure_current_user, only: %i[edit update destroy]
+  before_action :authenticate_user!, only: %i[new create show edit]
+
+  def index
+    @study_records = params[:tag_id].present? ? Tag.find(params[:tag_id]).study_records : StudyRecord.all
+  end
+
+  def new
+    @study_record = StudyRecord.new
+  end
+
+  def create
+    @study_record = StudyRecord.new(study_record_params)
+    if @study_record.save
+      redirect_to study_records_path, flash: { success: "学習記録の投稿に成功しました" }
+    else
+      flash.now[:danger] = "学習記録の投稿に失敗しました"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @study_record.update(study_record_params)
+      redirect_to study_record_path(@study_record), flash: { success: "学習記録の編集に成功しました" }
+    else
+      flash.now[:danger] = "学習記録の編集に失敗しました"
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @study_record.destroy
+    redirect_to study_records_path, status: :see_other, flash: { success: "学習記録の削除に失敗しました" }
+  end
+
+  private
+  def study_record_params
+    params.require(:study_record).permit(:name, :content, :user_id, tag_ids: [])
+  end
+
+  def set_current_user
+    @user = current_user
+  end
+
+  def set_study_record
+    @study_record = StudyRecord.find(params[:id])
+  end
+
+  def ensure_current_user
+    user = @study_record.user
+    unless current_user.id == user.id
+      redirect_to study_records_path
+    end
+  end
+end
